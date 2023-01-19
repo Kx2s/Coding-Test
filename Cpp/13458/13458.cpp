a09c65ddc38eb437c0bdb06e24920e7647ca31f3
N_name <105974690+Kx2s@users.noreply.github.com>
2023-01-19 11:05:00 +0900

    Add files via upload
    C++ 풀이

---
+++ Cpp/13458/13458.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, *A, B, C;    //시험장, 응시자, 총감독, 부감독
+    long int result = 0;
+    cin >> N;
+    A = new int[N];
+
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+    cin >> B >> C;
+
+    for (int i = 0; i < N; i++) {
+        int temp = A[i] - B;
+        result++;
+
+        if (temp > 0)
+            result += (temp % C == 0 ? temp / C : (temp / C) + 1);
+    }
+    cout << result;
+    return 0;
+}
---
+++ Cpp/14501/14501.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+#define MAX 16
+
+int N;
+int T[MAX];
+int P[MAX];
+int dp[MAX];
+
+int main() {
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+        cin >> T[i] >> P[i];
+
+    for (int i = N; i >= 1; i--) {
+        if (i + T[i] - 1 > N) {
+            dp[i] = dp[i + 1];
+            continue;
+        }
+        dp[i] = max(dp[i + T[i]] + P[i], dp[i + 1]);
+    }
+    cout << dp[1] << endl;
+}
