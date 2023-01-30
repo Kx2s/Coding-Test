19d16833472e664f02a290d5a73b4592c7e91817
Aringo <kib1773@naver.com>
2023-01-30 18:05:44 +0900

    2606 추가

---
+++ Cpp/2606/2606.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define MAX 101
+
+int net[MAX][MAX] = { 0 };
+bool tf[MAX] = { false };
+int run(int a);
+
+int main() {
+	int com, num, result = -1;
+
+	cin >> com;
+	cin >> num;
+
+	for (int i = 0; i < num; i++) {
+		int com1, com2;
+		cin >> com1 >> com2;
+
+		net[com1][com2] = 1;
+		net[com2][com1] = 1;
+	}
+	result += run(1);
+
+	cout << result << endl;
+}	
+
+int	run(int a) {
+	if (tf[a])
+		return 0;
+
+	tf[a] = true;
+	int result = 1;
+
+	for (int i = 0; i < MAX; i++) {
+		if (net[a][i]) {
+			result += run(i);
+		}
+	}
+	return result;
+}
