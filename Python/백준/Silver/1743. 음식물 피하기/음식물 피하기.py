95ac9f2a68d15a5c03179be03aabe6c3f83d2452
N_name <105974690+Kx2s@users.noreply.github.com>
2024-05-07 22:50:25 +0900

    [Silver I] Title: 음식물 피하기, Time: 1688 ms, Memory: 33916 KB -BaekjoonHub

--- Python/백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ Python/백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,22 +1,34 @@
+#다른 방식
 import sys
+sys.setrecursionlimit(10**6)
+
 def input() : return sys.stdin.readline()
 
+def dfs (x, y) :
+    visit[y][x] = 1
+    global size
+    size += 1
+
+    for mx, my in [[-1,0],[1,0],[0,-1],[0,1]] :
+        lx = x+mx; ly = y+my
+        if [lx,ly] in trash :
+            if not visit[ly][lx]:
+                dfs(lx,ly)
+
 N, M, K = map(int, input().split())
-li = []
+
+visit = [[0]*M for _ in range(N)]
+trash = []
+result = 0
 
 for _ in range(K) :
-    li.append(list(map(int, input().split())))
+    y,x = map(int, input().split())
+    trash.append([x-1,y-1])
 
-result = 0
-while len(li) > 0 :
-    l = li[0]
-    li.remove(l)
-    stack = [l]
-    for s in stack :
-        for l in li.copy() :
-            if abs(l[0] - s[0]) + abs(l[1] - s[1]) == 1 :
-                stack.append(l)
-                li.remove(l)
-    
-    result = max(result,len(stack))
+for x, y in trash :
+    if not visit[y][x] :
+        size = 0
+        dfs(x, y)
+        result = max(result, size)
+
 print(result)
