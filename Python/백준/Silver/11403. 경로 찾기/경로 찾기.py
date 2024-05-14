8414af06a2e2dfc136b816fd9ae1bf92a113ccae
N_name <105974690+Kx2s@users.noreply.github.com>
2024-05-14 16:22:15 +0900

    [Silver I] Title: 경로 찾기, Time: 204 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = []
+for _ in range(N) :
+    arr.append(list(map(int, input().split())))
+
+for k in range(N) :
+    for i in range(N) :
+        for j in range(N) :
+            if arr[i][k] and arr[k][j] :
+                arr[i][j] = 1
+
+for i in arr :
+    print(*i)
