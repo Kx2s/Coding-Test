830dcd1db9db437f62f9e4e6c1786687e2437341
N_name <105974690+Kx2s@users.noreply.github.com>
2024-07-01 01:44:48 +0900

    [Silver III] Title: 1로 만들기, Time: 136 ms, Memory: 18048 KB -BaekjoonHub

---
+++ Java/백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[N] = 0;
+
+        for (int i = N; i > 0; i--) {
+            if (i%2 == 0)
+                dp[i/2] = Math.min(dp[i/2], dp[i]+1);
+            if (i%3 == 0)
+                dp[i/3] = Math.min(dp[i/3], dp[i]+1);
+            dp[i-1] = Math.min(dp[i-1], dp[i]+1);
+        }
+        System.out.println(dp[1]);
+    }
+}
