f86d556bbe4d9b824768353ec561a03040d6d24d
N_name <105974690+Kx2s@users.noreply.github.com>
2024-08-25 21:07:40 +0900

    [Silver V] Title: 젓가락, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ Java/백준/Silver/24228. 젓가락/젓가락.java
@@ -0,0 +1,15 @@
+//https://www.notion.so/dcc890433be844008c2999401cfd2f57
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long N = Long.parseLong(st.nextToken());
+        long K = Long.parseLong(st.nextToken());
+
+        System.out.println(K*2 + N-1);
+    }
+}
