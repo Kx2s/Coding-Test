4c3f911814b50ab49b842a2bdc3f8ffcd91d42de
N_name <105974690+Kx2s@users.noreply.github.com>
2024-09-22 23:47:36 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 77.9 MB -BaekjoonHub

--- Java/프로그래머스/2/42586. 기능개발/기능개발.java
+++ Java/프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,3 +1,4 @@
+//https://www.notion.so/dcc890433be844008c2999401cfd2f57
 import java.util.*;
 
 class Solution {
@@ -6,7 +7,10 @@ class Solution {
         int day=1;
         int idx=0;
         int cnt=0;
+
+        //배포준비
         while (idx < progresses.length) {
+            //아직 작업이 안끝났을때
             if (progresses[idx] + speeds[idx]*day < 100){
                 day++;
                 if (cnt!=0){
@@ -19,10 +23,11 @@ class Solution {
             idx++;
         }
         ans.add(cnt);
-        
+
+        //변환
         int[] result = new int[ans.size()];
         for (int i=0; i<result.length; i++)
-            result[i] = ans.get(i);    
+            result[i] = ans.get(i);
         return result;
     }
 }
